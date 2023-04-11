015e9e59ba3d68f3cff45d9820302c301fd3182d
JakMar17 <jakob.marusic17@gmail.com>
2023-04-11 17:33:26 +0200

    added mobile navbar

--- components/navigation/Navbar.vue
+++ components/navigation/Navbar.vue
@@ -1,51 +1,73 @@
 <template>
-  <nav class="navbar" role="navigation" style="height: 6em" aria-label="main navigation">
-  <div class="navbar-brand">
-    <div class="level-item has-text-centered">
-      <nuxt-link to="/" class="navbar-item">
-        <img
-          class="is-centered image"
-          style="min-height: 5em"
-          src="@/assets/images/svgs/logo-sss.svg"
-        />
-        <div style="margin-left: 1vw">
-          <div class="has-text-weight-bold">
-            Študentski svet stanovalcev
+  <nav id="navMenu" class="navbar" role="navigation" style="height: 6em" aria-label="main navigation">
+    <div class="navbar-brand is-vcentered" style="display: flex; align-items: center">
+      <div class="level-item has-text-centered">
+        <nuxt-link to="/" class="navbar-item">
+          <img class="is-centered image" style="min-height: 5em" src="@/assets/images/svgs/logo-sss.svg"/>
+          <div style="margin-left: 1vw">
+            <div class="has-text-weight-bold">
+              Študentski svet stanovalcev
+            </div>
           </div>
-        </div>
-      </nuxt-link>
+        </nuxt-link>
+      </div>
+      <a role="button" @click="this.toggleMobileMenu()" :class="isMobileMenuActive" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-  </div>
 
-  <div class="navbar-menu">
-        <div class="navbar-end">
-      <div class="navbar-item">
-        <NuxtLink to="/" class="navbar-item">
-        Majske igre
-      </NuxtLink>
-      </div>
-      <div class="navbar-item">
-        <NuxtLink to="/sss/clani" class="navbar-item">
-        Kdo smo
-      </NuxtLink>
-      </div>
-      <div class="navbar-item">
-        <NuxtLink to="/projekti" class="navbar-item">
-        Projekti
-      </NuxtLink>
-      </div>
-      <div class="navbar-item">
-        <NuxtLink to="/contact" class="navbar-item">
-        Kontakt
-      </NuxtLink>
+    <div class="navbar-menu" :class="isMobileMenuActive">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <NuxtLink to="/" class="navbar-item">
+            Majske igre
+          </NuxtLink>
+        </div>
+        <div class="navbar-item">
+          <NuxtLink to="/sss/clani" class="navbar-item">
+            Kdo smo
+          </NuxtLink>
+        </div>
+        <div class="navbar-item">
+          <NuxtLink to="/projekti" class="navbar-item">
+            Projekti
+          </NuxtLink>
+        </div>
+        <div class="navbar-item">
+          <NuxtLink to="/contact" class="navbar-item">
+            Kontakt
+          </NuxtLink>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      activeMobileMenu: false
+    };
+  },
+  methods: {
+    toggleMobileMenu() {
+      this.activeMobileMenu = !this.activeMobileMenu;
+    }
+  },
+  computed: {
+    isMobileMenuActive() {
+      return this.activeMobileMenu ? 'is-active' : '';
+    }
+  },
+  watch: {
+    $route() {
+      this.activeMobileMenu = false;
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
