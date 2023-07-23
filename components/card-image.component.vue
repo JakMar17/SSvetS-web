e18a2a84fcc29a14e8dd10544f56d73768a4cfad
JakMar17 <jakob.marusic17@gmail.com>
2023-07-23 15:08:43 +0200

    dynamic height in card

--- components/card-image.component.vue
+++ components/card-image.component.vue
@@ -1,7 +1,7 @@
 <template>
-    <section :class="buildCssClass('wrapper')">
-        <div class="content">
-            <div class="content__card">
+    <section id="wrapper" ref="wrapper" :class="buildCssClass('wrapper')">
+        <div ref="content" class="content">
+            <div ref="content__card" class="content__card">
                 <h1 class="title is-2 mb-0">{{ title }}</h1>
                 <h2 v-if="subtitle" class="title is-6 mt-2 has-text-primary">{{ subtitle.toUpperCase() }}</h2>
                 <slot></slot>
@@ -9,7 +9,7 @@
         </div>
         <div :class="buildCssClass('background')">
             <div></div>
-            <img :src="imageUrl"/>
+            <img ref="backgroundImage" :src="imageUrl"/>
         </div>
     </section>
 </template>
@@ -33,9 +33,37 @@ export default {
             default: false
         }
     },
+    data() {
+        return {
+            clientWidth: null,
+            clientHeight: null
+        }
+    },
+    mounted() {
+        window.addEventListener("resize", () => this.onResize());
+        this.onResize();
+    },
     methods: {
         buildCssClass(baseClass = "") {
             return `${baseClass} ${this.reversed ? "reversed" : ""}`;
+        },
+        onResize() {
+            const clientWidth = this.$refs.wrapper?.clientWidth ?? 0;
+            const contentCardHeight = this.pxToEm(this.$refs.content__card?.offsetHeight);
+            const imageHeight = this.pxToEm(this.$refs.backgroundImage?.offsetHeight);
+
+            if (clientWidth <= 1024) {
+                const wrapper = this.$refs.wrapper;
+                console.log(wrapper)
+                console.log(wrapper.style)
+                if (wrapper.style) {
+                    wrapper.style.height = `${contentCardHeight + (imageHeight)}em`;
+                }
+            }
+        },
+        pxToEm(px = 0) {
+            const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+            return px / fontSize;
         },
     }
 }
@@ -92,8 +120,8 @@ export default {
     z-index: 1;
     width: 100%;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: flex-start;
 
     margin-left: 4em;
     margin-right: 4em;
@@ -102,13 +130,17 @@ export default {
     @media only screen and (min-width: 1024px) {
         margin-bottom: 0;
         justify-content: space-between;
+        align-items: center;
     }
 
     img {
         object-fit: cover;
         height: 35em;
+        border-radius: 1em 1em 0 0;
 
-        border-radius: 1em;
+        @media only screen and (min-width: 1024px) {
+            border-radius: 1em;
+        }
     }
 }
 
