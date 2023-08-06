1e73aedf3f5673ef5881a3a84039f18db477f546
JakMar17 <jakob.marusic17@gmail.com>
2023-08-06 16:16:26 +0200

    decapcms for sss

--- pages/sss/kdo-smo.vue
+++ pages/sss/kdo-smo.vue
@@ -1,90 +1,36 @@
 <template>
-  <main>
+    <main>
 
-    <SssHeaderComponent :links="headerLinks" imageUrl="https://www.stud-dom-lj.si/fileadmin/user_upload/Slike/Domovi/Dom_5/Dom5-DSC_6156.jpg" subtitle="Kdo smo in kaj je naša naloga?" title="Študentski svet stanovalcev"></SssHeaderComponent>
+        <SssHeaderComponent
+            :image-url="content.cover"
+            :subtitle="content.subtitle"
+            :title="content.title"
+        ></SssHeaderComponent>
 
-    <div class="container">
-      <div class="card p-4">
-        <p class="content">Javni zavod Študentski dom Ljubljana (ŠDL) sestavlja 29 študentskih domov, ki so postopoma
-          zrasli na različnih lokacijah po Ljubljani (Vič, Bežigrad, Center in Šiška). V njih biva nekaj manj kot 7500
-          stanovalcev in stanovalk.</p>
-        <p class="content">Študentski svet stanovalcev (ŠSS) je avtonomen organ, ki prek predstavnikov zastopa vse
-          stanovalce ŠDL. Vsak dom zastopa predstavnik, ki interese stanovalcev predstavlja na upravi ŠDL ter na sejah
-          ŠSS. ŠSS ima svojega predstavnika v svetu zavoda ŠDL, ki je najvišji organ</p>
-        <p class="content">V razprave izven zavoda se ŠSS vključi, kadar se te tičejo študentskega bivanja v širšem. Tu si
-          prizadevamo za korektno in enakovredno obravnavo študentskega bivanja v javnih študentskih domovih. Verjamemo,
-          da je bivanje, ki je prilagojeno mladim eden izmed ključnih delov študijskega procesa.</p>
-      </div>
-    </div>
+        <div class="container">
+            <div class="card p-4 mb-4">
+                <p class="pre-wrap">
+                    {{ content.intro }}
+                </p>
+            </div>
+        </div>
 
-    <TitleSub title="Delovanje Študentskega sveta stanovalcev" />
-    <div class="container">
-      <div class="card p-4">
-        <p class="content">ŠSS sestavlja 29 neposredno izvoljenih predstavnikov domov in 29 njihovih namestnikov.
-          Predstavniki so voljeni na volitvah v njihovem domu vsakega oktobra.</p>
-        <p class="content">Predstavnik in namestnik doma sta odgovorna za teme, ki se tičejo posameznih domov in tam
-          urejata bivanje ter zbirata pobude stanovalcev. Namestnik doma je imenovan s strani predstavnika in potrjen
-          na seji ŠSS.</p>
-        <p class="content">ŠSS kot organ deluje na sejah, kjer ima vsak dom en glas. Dom glas uveljavlja prek
-          predstavnika doma, v primeru njegove odsotnosti pa prek namestnika. Seje vodi predsednik ŠSS, ki je vsakega
-          oktobra neposredno izvoljen izmed članov ŠSS.</p>
-        <p class="content">Na sejah člani ŠSS sprejemajo skupna stališča glede tem, ki se tičejo bivanja v ŠDL ter
-          podajajo mnenja na bivanjsko problematiko v študentskih domovih, če je to potrebno.</p>
-      </div>
-    </div>
+        <CardImageComponent v-for="(card, index) in content.paragraphs" :key="card.title" :image-url="card.image" :reversed="index % 2 !== 0" :title="card.title">
+            <p>
+                {{ card.description }}
+            </p>
+        </CardImageComponent>
 
-    <TitleSub title="Domsko življenje" />
-    <div class="container">
-      <div class="card is-fullheight p-4">
-        <p class="content">Poleg uradnih dolžnosti predstavniki domov skrbijo, da v domu življenje gladko poteka.
-          Skrbijo za sredstva doma in skupaj s hišnikom doma urejajo skupne prostore ter skupni inventar. So prvi
-          naslov za bruce in preostale stanovalce, ki potrebujejo nasvet ali dva glede bivanja. Prav tako pa znotraj
-          doma organizirajo različne dogodke za stanovalce.</p>
-        <p class="content">Dodatno predstavniki in namestniki domov pomagajo pri organizaciji večjih projektov ŠSS.
-        </p>
-      </div>
-    </div>
-
-  </main>
+    </main>
 </template>
 
 
 <script>
 export default {
     async setup() {
-        const {data} = await useAsyncData('sss', async () => (await queryContent('sss/studentski-svet-stanovalcev').findOne()));
-        console.log(data)
+        const {data} = await useAsyncData('sss', async () => (await queryContent('sss/description').find())[0]);
         return {content: data.value}
     },
-    data() {
-        return {
-            headerLinks: [
-                {
-                    label: "Kdo smo",
-                    onClick: () => this.$router.push('/sss/kdo-smo')
-                },
-                {
-                    label: "Člani",
-                    onClick: () => this.$router.push('/sss/clani')
-              },
-              {
-                  label: "Organi",
-                  onClick: () => this.$router.push('/sss/organi')
-              },
-              {
-                  label: "Zgodovina",
-                  onClick: () => this.$router.push('/sss/zgodovina')
-              }
-          ]
-      }
-  },
-  async asyncData() {
-
-  },
-  async mounted() {
-
-  },
-  methods: {},
 }
 </script>
 
