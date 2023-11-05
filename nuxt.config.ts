441cd80ab599212a9221fb0c99e1eb5b45f0196c
JakMar17 <jakob.marusic17@gmail.com>
2023-11-05 19:29:45 +0100

    added background image, lazy loading images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: true,
     css: [
         "@/assets/styles/main.scss",
         "@/node_modules/remixicon/fonts/remixicon.css"
