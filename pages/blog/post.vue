cdb28b4930871fa6e50470ec426c4826c7ea9ebc
JakMar17 <jakob.marusic17@gmail.com>
2023-11-07 10:27:06 +0100

    blog post component

---
+++ pages/blog/post.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="card p-6">
+            <div class="has-text-centered">
+                <h1 class="title is-2 mb-0">Hello world</h1>
+                <h2 class="title is-6 mt-2 has-text-primary">PODNASLOV HELLO WORLDA</h2>
+                <div>
+                    <img alt="blog" src="/images/blog.jpg" style="height: 3em; width: 3em; border-radius: 50%; object-fit: cover"/>
+                    <div>Jakob Marušič</div>
+                </div>
+                <span class="help">Sobota, 4. oktober 2023</span>
+            </div>
+            <div class="has-text-weight-bold py-4">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
