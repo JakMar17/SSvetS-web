a11839cbd8a32c55b4adc5d677706f7153198712
JakMar17 <jakob.marusic17@gmail.com>
2023-11-12 19:30:35 +0100

    fixed projects

--- components/navigation/Navbar.vue
+++ components/navigation/Navbar.vue
@@ -1,72 +1,72 @@
 <template>
-  <nav id="navMenu" aria-label="main navigation" class="navbar" role="navigation">
-    <div class="navbar-brand is-vcentered is-flex is-align-items-center">
-      <div class="level-item has-text-centered">
-        <nuxt-link to="/" class="navbar-item">
-          <img class="is-centered image logo" src="@/assets/images/svgs/logo-sss.svg"/>
-          <div class="ml-2">
-            <div class="has-text-weight-bold">
-              Študentski svet stanovalcev
+    <nav id="navMenu" aria-label="main navigation" class="navbar" role="navigation">
+        <div class="navbar-brand is-vcentered is-flex is-align-items-center">
+            <div class="level-item has-text-centered">
+                <nuxt-link class="navbar-item" to="/">
+                    <img class="is-centered image logo" src="@/assets/images/svgs/logo-sss.svg"/>
+                    <div class="ml-2">
+                        <div class="has-text-weight-bold">
+                            Študentski svet stanovalcev
+                        </div>
+                    </div>
+                </nuxt-link>
             </div>
-          </div>
-        </nuxt-link>
-      </div>
-      <a role="button" @click="this.toggleMobileMenu()" :class="isMobileMenuActive" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+            <a :class="isMobileMenuActive" aria-expanded="false" aria-label="menu" class="navbar-burger" data-target="navMenu" role="button" @click="this.toggleMobileMenu()">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
 
-    <div class="navbar-menu" :class="isMobileMenuActive">
-      <div class="navbar-end">
-          <!--        <div class="navbar-item">-->
-          <!--          <NuxtLink to="/" class="navbar-item">-->
-          <!--            Majske igre-->
-          <!--          </NuxtLink>-->
-          <!--        </div>-->
-          <div class="navbar-item">
-          <NuxtLink to="/sss/clani" class="navbar-item">
-            Kdo smo
-          </NuxtLink>
+        <div :class="isMobileMenuActive" class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <NuxtLink class="navbar-item" to="/sss/clani">
+                        Kdo smo
+                    </NuxtLink>
+                </div>
+                <div class="navbar-item">
+                    <NuxtLink class="navbar-item" to="/projekti">
+                        Projekti
+                    </NuxtLink>
+                </div>
+                <div class="navbar-item">
+                    <NuxtLink class="navbar-item" to="/blog">
+                        Novice
+                    </NuxtLink>
+                </div>
+                <div class="navbar-item">
+                    <NuxtLink class="navbar-item" to="/kontakt">
+                        Kontakt
+                    </NuxtLink>
+                </div>
+            </div>
         </div>
-        <div class="navbar-item">
-          <NuxtLink to="/projekti" class="navbar-item">
-            Projekti
-          </NuxtLink>
-        </div>
-        <div class="navbar-item">
-          <NuxtLink class="navbar-item" to="/kontakt">
-              Kontakt
-          </NuxtLink>
-        </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      activeMobileMenu: false
-    };
-  },
-  methods: {
-    toggleMobileMenu() {
-      this.activeMobileMenu = !this.activeMobileMenu;
+    data() {
+        return {
+            activeMobileMenu: false
+        };
+    },
+    methods: {
+        toggleMobileMenu() {
+            this.activeMobileMenu = !this.activeMobileMenu;
+        }
+    },
+    computed: {
+        isMobileMenuActive() {
+            return this.activeMobileMenu ? 'is-active' : '';
+        }
+    },
+    watch: {
+        $route() {
+            this.activeMobileMenu = false;
+        }
     }
-  },
-  computed: {
-    isMobileMenuActive() {
-      return this.activeMobileMenu ? 'is-active' : '';
-    }
-  },
-  watch: {
-    $route() {
-      this.activeMobileMenu = false;
-    }
-  }
 }
 </script>
 
@@ -74,7 +74,7 @@ export default {
 @import '@/assets/styles/main.scss';
 
 nav {
-  height: 6em;
+    height: 6em;
 }
 
 .navbar {
@@ -83,6 +83,6 @@ nav {
 }
 
 .logo {
-  min-height: 5em;
+    min-height: 5em;
 }
 </style>
--- pages/projekti.vue
+++ pages/projekti.vue
@@ -1,6 +1,6 @@
 <template>
     <main v-if="componentState === 'loaded'">
-        <ProjectsHeaderComponent></ProjectsHeaderComponent>
+        <HeaderPageComponent :imageUrl="imageUrl" subtitle="Šport, zabava & kultura" title="Projekti Študentskega sveta stanovalcev"/>
         <CardComponent :content="content.intro"></CardComponent>
         <CardImageComponent v-for="(project, index) in content.projects" :key="project.title" :content="project.description" :image-url="project.image" :links="project.links" :reversed="index % 2 !== 0" :subtitle="project.subtitle" :title="project.title"/>
     </main>
@@ -12,6 +12,7 @@ export default {
         this.fetchData();
         return {
             content: null,
+            imageUrl: null,
             componentState: 'loading'
         }
     },
@@ -20,6 +21,7 @@ export default {
             try {
                 const data = await queryContent('pages/projects').findOne();
                 this.content = data ?? null;
+                this.imageUrl = data.featureImage ?? null;
                 this.componentState = 'loaded';
             } catch (e) {
                 this.componentState = 'error';
